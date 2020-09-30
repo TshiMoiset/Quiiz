638f057076d98b7bb1d96b5d2de41b77baf90f1b
Moise Tshibwabwa <moise.tshibwabwa@learnet.se>
2020-09-30 19:22:44 +0200

    Save: Typewrite

--- Quiiz/Program.cs
+++ Quiiz/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 
 namespace Test
 {
@@ -11,20 +12,32 @@ namespace Test
 
             Console.ForegroundColor
                                  = ConsoleColor.Yellow;
-            Console.WriteLine("Hej! Jag heter Uwuwewewe onyetenwewe ugweuhem osas. Välkommen till mitt frågesport.");
-            Console.WriteLine("Är du redo?");
+            Typewrite("Hej! Jag heter Uwuwewewe onyetenwewe ugweuhem osas. Välkommen till mitt frågesport.");
+            Console.WriteLine();
+            Typewrite("Är du redo?");
+            Console.WriteLine();
             Svar = Console.ReadLine().ToLower();
 
 
             if (Svar == "ja")
             {
                 Console.WriteLine();
-                Console.WriteLine("Strålande!! Reglerna är enkla");
-                Console.WriteLine("Jag ställer en fråga och du får 3 alternativ att välja mellan.");
-                Console.WriteLine("Varje fråga ger olika mycket poäng beroende på frågans svårighetsnivå.");
-                Console.WriteLine("Dessutom kommer fråga att visa hur mycket poäng man får.");
-                Console.WriteLine("Dina antal poäng kommer i slutet att ge dig ett resultat i form av ett betyg A - F, då A är bäst och F för FALIUER.");
-                Console.WriteLine("Got it?");
+                Typewrite("Strålande!! Reglerna är enkla");
+                Console.WriteLine();
+
+                Typewrite("Jag ställer en fråga och du får 3 alternativ att välja mellan.");
+                Console.WriteLine();
+
+                Typewrite("Varje fråga ger olika mycket poäng beroende på frågans svårighetsnivå.");
+                Console.WriteLine();
+
+                Typewrite("Dessutom kommer fråga att visa hur mycket poäng man får.");
+                Console.WriteLine();
+
+                Typewrite("Dina antal poäng kommer i slutet att ge dig ett resultat i form av ett betyg A - F, då A är bäst och F för FALIUER.");
+                Console.WriteLine();
+                Typewrite("Got it?");
+                Console.WriteLine();
                 Redo();
             }
 
@@ -32,22 +45,22 @@ namespace Test
 
             {
                 Console.WriteLine();
-                Console.WriteLine("Ok!! Bry mig inte om du är redo, vi sätter igång ändå.");
-                Console.WriteLine("Reglerna är enkla, jag ställer en fråga och du får 3 alternativ att välja mellan.");
-                Console.WriteLine("Frågorna ger olika mycket poäng beroende på frågans svårighet. Varje fråga kommer att visa hur mycket poäng man får");
-                Console.WriteLine("Dina antal poäng kommer i slutet att ge dig ett resultat i form av ett betyg A - F, då A är bäst och F för FALIUER.");
-                Console.WriteLine("Got it?");
-                Redo();
+                Typewrite("Ok!! Bry mig inte om du är redo, vi sätter igång ändå.");
+                Console.WriteLine();
 
-                if (Svar == "ok")
-                {
-                    Console.WriteLine();
-                    Console.WriteLine("Mmmm..........ok!!");
-                    Console.WriteLine("Reglerna är enkla, jag ställer en fråga och du frå 3 alternativ. Rätt svar ger olika mycket poäng. Varje fråga kommer att visa hur mycket poöng man får.");
-                    Console.WriteLine("Dina antal poäng kommer att ge dig ett resultat i form av ett betyg A - F, då A är bäst och F för FALIUER.");
-                    Console.WriteLine("Got it?");
-                    Redo();
-                }
+                Typewrite("Reglerna är enkla, jag ställer en fråga och du får 3 alternativ att välja mellan.");
+                Console.WriteLine();
+
+                Typewrite("Frågorna ger olika mycket poäng beroende på frågans svårighet. Varje fråga kommer att visa hur mycket poäng man får");
+                Console.WriteLine();
+
+                Typewrite("Dina antal poäng kommer i slutet att ge dig ett resultat i form av ett betyg A - F, då A är bäst och F för FALIUER.");
+                Console.WriteLine();
+
+                Typewrite("Got it?");
+                Console.WriteLine();
+
+                Redo();
 
             }
 
@@ -66,13 +79,17 @@ namespace Test
             if (Svar2 == "ja")
             {
                 Console.WriteLine();
-                Console.WriteLine("Då kör vi.");
+                Typewrite("Då kör vi.");
+                Console.WriteLine();
+
                 betyg();
             }
             if (Svar2 == "nej")
             {
                 Console.WriteLine();
-                Console.WriteLine("Vi sätter igång ändå!!");
+                Typewrite("Vi sätter igång ändå!!");
+                Console.WriteLine();
+
                 betyg();
             }
             Console.ReadLine();
@@ -85,70 +102,121 @@ namespace Test
             Hp = 5000;
             String Svar3;
 
-            Console.WriteLine("Snabb fråga innan vi börjar vilket betyg tror du att du kommer få från A - F?");
+            Typewrite("Snabb fråga innan vi börjar vilket betyg tror du att du kommer få från A - F?");
             Console.WriteLine();
-            Console.WriteLine("A = " + Hp + " p");
-            Console.WriteLine("B = " + (Hp - 1250) + " p");
-            Console.WriteLine("C = " + (Hp - 2500) + " p");
-            Console.WriteLine("D = " + (Hp - 3250) + " p");
-            Console.WriteLine("E = " + (Hp - 3750) + " p");
-            Console.WriteLine("F = " + (Hp - 5000) + " p");
+            Typewrite("A = " + Hp + " p");
+            Console.WriteLine();
+            Typewrite("B = " + (Hp - 1250) + " p");
+            Console.WriteLine();
+
+            Typewrite("C = " + (Hp - 2500) + " p");
+            Console.WriteLine();
+
+            Typewrite("D = " + (Hp - 3250) + " p");
+            Console.WriteLine();
+
+            Typewrite("E = " + (Hp - 3750) + " p");
+            Console.WriteLine();
+
+            Typewrite("F = " + (Hp - 5000) + " p");
+            Console.WriteLine();
+
 
             Svar3 = Console.ReadLine().ToLower();
 
             if (Svar3 == "a")
             {
                 Console.WriteLine();
-                Console.WriteLine("Hahahahah!!!! DU TROR? Hahaha!! OK, OK");
-                Console.WriteLine("Vi sätter igång.");
-                Console.WriteLine("Tryck ENTER för att starta!");
+                Typewrite("Hahahahah!!!! DU TROR? Hahaha!! OK, OK");
+                Console.WriteLine();
+
+                Typewrite("Vi sätter igång.");
+                Console.WriteLine();
+
+                Typewrite("Tryck ENTER för att starta!");
+                Console.WriteLine();
+
                 Fråga1();
             }
 
             if (Svar3 == "b")
             {
                 Console.WriteLine();
-                Console.WriteLine("Hahahaha!!! Du kommer knappt att klara första frågan!!");
-                Console.WriteLine("Men vi kör, vi kör. Visa mig att jag har fel.");
-                Console.WriteLine("Jag har ALDRIG fel!!");
-                Console.WriteLine("Tryck ENTER för att starta!");
+                Typewrite("Hahahaha!!! Du kommer knappt att klara första frågan!!");
+                Console.WriteLine();
+
+                Typewrite("Men vi kör, vi kör. Visa mig att jag har fel.");
+                Console.WriteLine();
+
+                Typewrite("Jag har ALDRIG fel!!");
+                Console.WriteLine();
+
+                Typewrite("Tryck ENTER för att starta!");
+                Console.WriteLine();
+
                 Fråga1();
             }
 
             if (Svar3 == "c")
             {
                 Console.WriteLine();
-                Console.WriteLine("Ok, men varför inte ett A? Satsar du så lågt haha!!!");
-                Console.WriteLine("Ok, ok vi sätter igång!");
-                Console.WriteLine("Tryck ENTER för att starta!");
+                Typewrite("Ok, men varför inte ett A? Satsar du så lågt haha!!!");
+                Console.WriteLine();
+
+                Typewrite("Ok, ok vi sätter igång!");
+                Console.WriteLine();
+
+                Typewrite("Tryck ENTER för att starta!");
+                Console.WriteLine();
+
                 Fråga1();
             }
 
             if (Svar3 == "d")
             {
                 Console.WriteLine();
-                Console.WriteLine("Ok, någon satsar lågt.");
-                Console.WriteLine("Det kanske går bättre än vad man förväntar sig");
-                Console.WriteLine("Tryck ENTER för att starta!");
+                Typewrite("Ok, någon satsar lågt.");
+                Console.WriteLine();
+
+                Typewrite("Det kanske går bättre än vad man förväntar sig");
+                Console.WriteLine();
+
+                Typewrite("Tryck ENTER för att starta!");
+                Console.WriteLine();
+
                 Fråga1();
             }
 
             if (Svar3 == "e")
             {
                 Console.WriteLine();
-                Console.WriteLine("Varför satsar du så lågt? Man ska satsa högt.");
-                Console.WriteLine("Man ska sikta på molnen");
-                Console.WriteLine("Men ok, Vi kör.");
-                Console.WriteLine("Tryck ENTER för att starta!");
+                Typewrite("Varför satsar du så lågt? Man ska satsa högt.");
+                Console.WriteLine();
+
+                Typewrite("Man ska sikta på molnen");
+                Console.WriteLine();
+
+                Typewrite("Men ok, Vi kör.");
+                Console.WriteLine();
+
+                Typewrite("Tryck ENTER för att starta!");
+                Console.WriteLine();
+
                 Fråga1();
             }
 
             if (Svar3 == "f")
             {
                 Console.WriteLine();
-                Console.WriteLine("Mmm........:/");
-                Console.WriteLine("Aha starta ok. Det var väll det du sa ok. Vi sätter igång.");
-                Console.WriteLine("Tryck ENTER för att starta!");
+                Typewrite("Mmm........:/");
+                Console.WriteLine();
+
+                Typewrite("Aha starta ok. Det var väll det du sa ok. Vi sätter igång.");
+                Console.WriteLine();
+
+                Typewrite("Tryck ENTER för att starta!");
+                Console.WriteLine();
+
                 Fråga1();
             }
 
@@ -165,28 +233,42 @@ namespace Test
 
             Console.ReadLine();
             Console.WriteLine();
-            Console.WriteLine("Fråga1:" + Hp + " Poäng");
+            Typewrite("Fråga1:" + Hp + " Poäng");
             Console.WriteLine();
-            Console.WriteLine("Vad heter USAs första president?");
-            Console.WriteLine("1. George Washington");
-            Console.WriteLine("X. Geogre Washington");
-            Console.WriteLine("2. George Wasington");
+            Typewrite("Vad heter USAs första president?");
+            Console.WriteLine();
+
+            Typewrite("1. George Washington");
+            Console.WriteLine();
+
+            Typewrite("X. Geogre Washington");
+            Console.WriteLine();
+
+            Typewrite("2. George Wasington");
+            Console.WriteLine();
+
             Svar4 = Console.ReadLine().ToLower();
 
             if (Svar4 == "1")
             {
                 Console.WriteLine();
-                Console.WriteLine("Korrekt Svar.");
-                Console.WriteLine(Hp + " p");
+                Typewrite("Korrekt Svar.");
+                Console.WriteLine();
+
+                Typewrite(Hp + " p");
             }
 
             else if (Svar4 != "1")
             {
                 Console.WriteLine();
-                Console.WriteLine("Felaktigt Svar");
-                Console.WriteLine("Hahah!!");
+                Typewrite("Felaktigt Svar");
+                Console.WriteLine();
 
-                Console.WriteLine(Hp - 500 + " p");
+                Typewrite("Hahah!!");
+                Console.WriteLine();
+
+                Typewrite(Hp - 500 + " p");
+                Console.WriteLine();
 
             }
 
@@ -198,9 +280,9 @@ namespace Test
         {
             string svar5;
 
-            Console.WriteLine("Fråga 2: XX Poäng");
+            Typewrite("Fråga 2: XX Poäng");
             Console.WriteLine();
-            Console.WriteLine();
+            Typewrite("Fråga inte tillgänlig");
 
 
             svar5 = Console.ReadLine().ToLower();
@@ -208,6 +290,40 @@ namespace Test
 
 
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        static void Typewrite(string message)
+        {
+            for (int i = 0; i < message.Length; i++)
+            {
+                Console.Write(message[i]);
+                System.Threading.Thread.Sleep(30);
+            }
+        }
     }
 }
-
